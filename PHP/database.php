97e6146bf2ad99546e07f361b1487c86b1452e23
adelbecsi <adelbecsi@gmail.com>
2022-03-20 18:09:19 +0100

    Update database.php

--- PHP/database.php
+++ PHP/database.php
@@ -1,13 +1,29 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "y2022";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) { 
-    die("Kapcsolódás sikertelen: " . $conn->connect_error);
+function open_connection() {
+    $servername = "localhost";
+    $username = "petparadise";
+    $password = "petparadise";
+    $dbname = "y2022";
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) { 
+        die("Connecting failed: " . $conn->connect_error);
+    }
+    else{
+        echo("Successful connection");
+    }
+
+    return $conn;
 }
+ function close_connection($conn){
+    $conn->close();
+ }
 
-$conn->close();
+ $conn= open_connection();
+ close_connection($conn);
+
+ function 
+
+
 ?>
